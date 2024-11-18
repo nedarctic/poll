1c14deae04836fdcfa5003f21c7598a45c9e1d07
nedarctic <nedarctic@gmail.com>
2024-11-18 18:18:47 +0300

    Configured the project to use environment variables

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&e=qq96hvzkocia##x03*e%foet*(9b30#_xa350@@671p&hnk'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = []
 
